61d9f0d0637d60748f010227d2ef95190c73610f
Benn Pörscke <Benn.Porscke@consafelogistics.com>
2017-08-04 09:07:31 +0200

    Updated to a newer electron-settings version
    Changed setup of default values

--- app-settings.js
+++ app-settings.js
@@ -7,6 +7,9 @@
 const settings = require('electron-settings');
 const path = require('path');
 
+const URL_PATH ='urlSetupPath';
+const SWITCH_TIME = 'switchTime';
+
 /**
  * Settinsg data
  * URL config file name and path
@@ -17,17 +20,15 @@ exports.addDefault = function () {
   // Add the default data to settings
   let setupPath = path.join(process.env['HOME'],'.visa', 'setup-url.conf')
   console.log('Default setup path = ' + setupPath);
-  settings.defaults({
-    urlSetupPath: setupPath,
-    switchTime: 15
-  });
+  settings.get( URL_PATH, setupPath);
+  settings.get( SWITCH_TIME, 15);
 }
 
 exports.saveSetupPath = function (setupFilePath) {
   // Saves the URL setup file path
 
-  settings.set('urlSetupPath', setupFilePath).then(() => {
-    settings.get('urlSetupPath').then(val => {
+  settings.set(URL_PATH, setupFilePath).then(() => {
+    settings.get(URL_PATH).then(val => {
       console.log('Save settings path = ' + val);
     });
   });
@@ -35,14 +36,14 @@ exports.saveSetupPath = function (setupFilePath) {
 
 exports.getSetupPath = function () {
   // Get the Astro Path from persistant persistant
-  console.log(settings.getSync('urlSetupPath'));
-  return settings.getSync('urlSetupPath');
+  console.log(settings.getSync(URL_PATH));
+  return settings.getSync(URL_PATH);
 }
 
 exports.saveSwitchTime = function (switchTime) {
   // Saves the Switch time
-  settings.set('switchTime', switchTime).then(() => {
-    settings.get('switchTime').then(val => {
+  settings.set(SWITCH_TIME, switchTime).then(() => {
+    settings.get(SWITCH_TIME).then(val => {
       console.log('Save settings switch = ' + val);
     });
   });
@@ -51,5 +52,5 @@ exports.saveSwitchTime = function (switchTime) {
 exports.getSwitchTime = function () {
   // Get the Switch Time
 
-  return settings.getSync('switchTime');
+  return settings.getSync(SWITCH_TIME);
 }
